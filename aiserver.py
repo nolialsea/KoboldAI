5552cb661f553521ef83498f3eb22de66119b186
Noli <nolialsea@gmail.com>
2022-03-29 09:57:26 +0200

    idk what I'm doing

--- aiserver.py
+++ aiserver.py
@@ -1781,7 +1781,7 @@ def index():
 def _proxy(*args, **kwargs):
     resp = requests.request(
         method=request.method,
-        url=request.url.replace(request.host_url, 'localhost:7319/'),
+        url=request.url.replace(request.host_url, 'http://localhost:7319/'),
         headers={key: value for (key, value) in request.headers if key != 'Host'},
         data=request.get_data(),
         cookies=request.cookies,
